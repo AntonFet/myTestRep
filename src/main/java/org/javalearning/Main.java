1c1633ab96bb13669e8743a422e8096606944655
AntonFet <48539443+AntonFet@users.noreply.github.com>
2024-02-18 14:33:40 +0700

    My latest commit

--- src/main/java/org/javalearning/Main.java
+++ src/main/java/org/javalearning/Main.java
@@ -5,6 +5,7 @@ package org.javalearning;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
     }
 }
